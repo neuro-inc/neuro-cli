976a2e3f6f57aeabe3b34a2cefef94e1622493c1
Ilya Gruzinov <igruzinov@yandex.ru>
2018-09-12 17:28:31 +0300

    Cover neuromation.logging by pytest

--- python/tests/test_logging.py
+++ python/tests/test_logging.py
@@ -1,24 +1,23 @@
-import re
 from logging import ERROR, WARNING, LogRecord
-
-import pytest
 
 from neuromation.logging import ConsoleWarningFormatter
 
 formatter = ConsoleWarningFormatter('%(name)s.%(funcName)s: %(message)s')
 
+
 def test_warning():
     record = LogRecord('n', WARNING, 'p', 1, 'warn-message', None, None)
     formatted = formatter.format(record)
-    #yellow WARNING
+    # yellow WARNING
     assert formatted.startswith('\x1b[33mWARNING\x1b[0m')
-    #message inside
+    # message inside
     assert formatted.find('warn-message') >= 0
+
 
 def test_error():
     record = LogRecord('n', ERROR, 'p', 1, 'error-message', None, None)
     formatted = formatter.format(record)
-    #red ERROR
+    # red ERROR
     assert formatted.startswith('\x1b[31mERROR\x1b[0m')
-    #message inside
+    # message inside
     assert formatted.find('error-message') >= 0
