cad0e6eb2ff9f3530139d9833c440d82a042c9a2
Rafael Zubairov <zubra.bubra@gmail.com>
2018-10-25 10:49:35 +0300

    Add event_loop to tested object.

--- python/tests/test_stream_reader.py
+++ python/tests/test_stream_reader.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 from neuromation.http.fetch import SyncStreamWrapper
 
 
-def test_stream_reading():
+def test_stream_reading(loop):
     initial_buf_values = bytearray(range(100, 0, -1))
     values_read_from_stream = bytearray(range(len(initial_buf_values)))
 
@@ -13,7 +13,7 @@ def test_stream_reading():
     underlying_stream = MagicMock()
     underlying_stream.read = read_op
 
-    wrapper = SyncStreamWrapper(None)
+    wrapper = SyncStreamWrapper(None, loop=loop)
     wrapper._stream_reader = underlying_stream
 
     my_buf = initial_buf_values
