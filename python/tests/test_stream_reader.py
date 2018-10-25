20da4cdd68c36281116b1012cbe6e292db1f2971
Rafael Zubairov <zubra.bubra@gmail.com>
2018-10-25 10:44:51 +0300

    Test stream reader operation.

---
+++ python/tests/test_stream_reader.py
@@ -0,0 +1,22 @@
+from unittest.mock import MagicMock
+
+from neuromation.http.fetch import SyncStreamWrapper
+
+
+def test_stream_reading():
+    initial_buf_values = bytearray(range(100, 0, -1))
+    values_read_from_stream = bytearray(range(len(initial_buf_values)))
+
+    async def read_op(arr_length):
+        return bytearray(range(arr_length))
+
+    underlying_stream = MagicMock()
+    underlying_stream.read = read_op
+
+    wrapper = SyncStreamWrapper(None)
+    wrapper._stream_reader = underlying_stream
+
+    my_buf = initial_buf_values
+    wrapper.readinto(my_buf)
+
+    assert my_buf == values_read_from_stream
