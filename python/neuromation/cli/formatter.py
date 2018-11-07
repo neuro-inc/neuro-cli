97b442c125f57c8f49c340d079516ee5265caf25
Rafael Zubairov <zubra.bubra@gmail.com>
2018-11-07 14:31:00 +0300

    Report job-container pending status if any provided.

--- python/neuromation/cli/formatter.py
+++ python/neuromation/cli/formatter.py
@@ -1,11 +1,11 @@
 from typing import Union
 
-from neuromation.client.jobs import JobDescription, JobItem
+from neuromation.client.jobs import JobDescription, JobItem, JobStatus
 
 
 class OutputFormatter:
     @classmethod
-    def format_job(cls, job: Union[JobItem, JobDescription], quiet: bool = True):
+    def format_job(cls, job: Union[JobItem, JobDescription], quiet: bool = True) -> str:
         if quiet:
             return job.id
         return (
@@ -15,3 +15,33 @@ class OutputFormatter:
             + f"  neuro job monitor {job.id} # monitor job stdout\n"
             + f"  neuro job kill {job.id}    # kill job"
         )
+
+
+class JobStatusFormatter:
+    @classmethod
+    def format_job_status(cls, job_status: JobDescription) -> str:
+        result: str = f"Job: {job_status.id}\n"
+        result += f"Status: {job_status.status}\n"
+        result += f"Image: {job_status.image}\n"
+        result += f"Command: {job_status.command}\n"
+        result += f"Resources: {job_status.resources}\n"
+
+        if job_status.url:
+            result = f"{result}Http URL: {job_status.url}\n"
+
+        result = f"{result}Created: {job_status.history.created_at}"
+        if job_status.status in [
+            JobStatus.RUNNING,
+            JobStatus.FAILED,
+            JobStatus.SUCCEEDED,
+        ]:
+            result += "\n" f"Started: {job_status.history.started_at}"
+        if job_status.status in [JobStatus.FAILED, JobStatus.SUCCEEDED]:
+            result += "\n" f"Finished: {job_status.history.finished_at}"
+        if job_status.status == JobStatus.PENDING and job_status.history.reason:
+            result += "\n" f"Reason: {job_status.history.reason}"
+        if job_status.status == JobStatus.FAILED:
+            result += "\n" f"Reason: {job_status.history.reason}\n"
+            result += "===Description===\n"
+            result += f"{job_status.history.description}\n================="
+        return result
