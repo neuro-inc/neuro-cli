61c270183eeab81c0baffc4c449a4f9e37ff56e5
Ilya Gruzinov <shagren@gmail.com>
2018-11-30 18:43:04 +0300

    Pypi distribution for python cli  (#251)
    * Pypi integration

--- python/setup.py
+++ python/setup.py
@@ -4,20 +4,22 @@
 
 from setuptools import find_packages, setup
 
-
 with open('README.md') as f:
     readme = f.read()
 
+# TODO: Add license
+license = None
 # with open('LICENSE') as f:
 #     license = f.read()
 
 setup(
     name='neuromation',
 
+    # TODO: decide where take/store versions
     use_scm_version={'root': '..', 'relative_to': __file__},
     setup_requires=['setuptools_scm'],
 
-    python_requires='>=3.5.0',
+    python_requires='>=3.6.0',
     # Make sure to pin versions of install_requires
     install_requires=[
         'aiohttp==3.4.4',
@@ -30,16 +32,34 @@ setup(
         'python-dateutil==2.7.5',
         'keyring~=13.0',
         'keyrings.cryptfile==1.2.1',
-        'tqdm==4.1.0',
+        'tqdm==4.28.1',
     ],
     include_package_data=True,
     description='Neuromation Platform API client',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Neuromation Team',
-    author_email='engineering@neuromation.io',
+    author_email='pypi@neuromation.io',  # TODO: change this email
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
-    entry_points={'console_scripts': [
-        'neuro=neuromation.cli:main'
-    ]}
+    url='https://neuromation.io/',
+    packages=find_packages(include=('neuromation', 'neuromation.*')),
+    entry_points={
+        'console_scripts': [
+            'neuro=neuromation.cli:main'
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "License :: Other/Proprietary License"
+    ],
 )
