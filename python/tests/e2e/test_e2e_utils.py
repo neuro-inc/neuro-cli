8010eb1fd2a645e11f829e3e69beffc071b83847
Artem Yushkovskiy <ajuszkowski@ya.ru>
2018-12-17 12:11:15 +0200

    change 1

--- python/tests/e2e/test_e2e_utils.py
+++ python/tests/e2e/test_e2e_utils.py
@@ -2,7 +2,7 @@ from time import sleep, time
 
 
 JOB_TIMEOUT = 60 * 5
-JOB_WAIT_SLEEP_SECONDS = 5
+JOB_WAIT_SLEEP_SECONDS = 2
 
 
 # TODO (R Zubairov, 09/13/2018): once we would have wait for job
@@ -29,10 +29,10 @@ def wait_for_job_to_change_state_to(run, job_id, str_target, str_stop=None):
     out = ""
     start_time = time()
     while str_target not in out:
+        sleep(JOB_WAIT_SLEEP_SECONDS)
         _, captured = run(["job", "status", job_id])
         out = captured.out
         if str_stop and str_stop in out:
             raise Exception(f"failed running job {job_id}: '{str_stop}' in '{out}'")
         if int(time() - start_time) > JOB_TIMEOUT:
             raise Exception(f"timeout exceeded, last output: '{out}'")
-        sleep(JOB_WAIT_SLEEP_SECONDS)
