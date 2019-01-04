f7e09244bfc04c4b9f588a7488b08934b8838df0
Ilya Gruzinov <igruzinov@yandex.ru>
2019-01-04 18:39:19 +0300

    Mark test as e2e

--- python/tests/e2e/test_e2e_share.py
+++ python/tests/e2e/test_e2e_share.py
@@ -1,5 +1,6 @@
+import pytest
 
-
+@pytest.mark.e2e
 def test_share_complete_lifecycle(run):
     captured = run(['share', 'storage:shared-read', 'public', 'read'])
     assert captured.out.strip() == 'Resource shared.'
