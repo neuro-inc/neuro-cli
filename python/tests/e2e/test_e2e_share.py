6c904ee209bca64fb63482b6fcbe469c6c464ebf
Ilya Gruzinov <igruzinov@yandex.ru>
2019-01-04 18:39:54 +0300

    Formatting issue

--- python/tests/e2e/test_e2e_share.py
+++ python/tests/e2e/test_e2e_share.py
@@ -1,8 +1,8 @@
 import pytest
 
+
 @pytest.mark.e2e
 def test_share_complete_lifecycle(run):
-    captured = run(['share', 'storage:shared-read', 'public', 'read'])
-    assert captured.out.strip() == 'Resource shared.'
+    captured = run(["share", "storage:shared-read", "public", "read"])
+    assert captured.out.strip() == "Resource shared."
     # TODO: Add revoke here
-
