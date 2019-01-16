b447210a9f028f4e310b3c4fb2e6859be2b06299
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-01-16 16:18:30 +0200

    Use --show-traceback in e2e tests

--- python/tests/e2e/conftest.py
+++ python/tests/e2e/conftest.py
@@ -76,7 +76,7 @@ def run(monkeypatch, capsys, tmpdir, setup_local_keyring):
     def _run(arguments):
         log.info("Run 'neuro %s'", " ".join(arguments))
         monkeypatch.setattr(Path, "home", _home)
-        monkeypatch.setattr(sys, "argv", ["nmc"] + arguments)
+        monkeypatch.setattr(sys, "argv", ["nmc"] + arguments + ["--show-traceback"])
 
         from neuromation.cli import main
 
