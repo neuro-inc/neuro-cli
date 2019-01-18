af94cd1f3ab4aac2c56449af1beaf328b1366b87
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-01-18 11:33:13 +0200

    Add test

---
+++ python/tests/clientv2/test_storage_filestatus.py
@@ -0,0 +1,48 @@
+from neuromation.clientv2 import FileStatus, FileStatusType
+
+
+def test_from_api():
+    stat = FileStatus.from_api(
+        {
+            "path": "path/to",
+            "type": "FILE",
+            "length": 1234,
+            "modificationTime": 3456,
+            "permission": "read",
+        }
+    )
+    assert stat.path == "path/to"
+    assert stat.type == FileStatusType.FILE
+    assert stat.size == 1234
+    assert stat.modification_time == 3456
+    assert stat.permission == "read"
+
+
+def test_file():
+    stat = FileStatus.from_api(
+        {
+            "path": "path/to",
+            "type": "FILE",
+            "length": 1234,
+            "modificationTime": 3456,
+            "permission": "read",
+        }
+    )
+    assert stat.type == FileStatusType.FILE
+    assert stat.is_file()
+    assert not stat.is_dir()
+
+
+def test_is_dir():
+    stat = FileStatus.from_api(
+        {
+            "path": "path/to",
+            "type": "DIRECTORY",
+            "length": 1234,
+            "modificationTime": 3456,
+            "permission": "read",
+        }
+    )
+    assert stat.type == FileStatusType.DIRECTORY
+    assert not stat.is_file()
+    assert stat.is_dir()
