f0738d4b1139253c4bf03e755015f71c2fa8117e
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-01-18 13:27:08 +0200

    Tune coverage ignore

--- python/neuromation/clientv2/abc.py
+++ python/neuromation/clientv2/abc.py
@@ -3,13 +3,13 @@ import abc
 
 class AbstractProgress(abc.ABC):
     @abc.abstractmethod
-    def start(self, file: str, size: int) -> None:
+    def start(self, file: str, size: int) -> None:  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def complete(self, file: str) -> None:
+    def complete(self, file: str) -> None:  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def progress(self, file: str, current: int) -> None:
+    def progress(self, file: str, current: int) -> None:  # pragma: no cover
         pass
