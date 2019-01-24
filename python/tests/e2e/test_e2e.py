48a9ae37388567b9059360c4c909c8ff2c8db7fc
Alex Davydow <alexey.davydov@neuromation.io>
2019-01-24 16:01:22 -0500

    added retries two job_storage interaction

--- python/tests/e2e/test_e2e.py
+++ python/tests/e2e/test_e2e.py
@@ -1,4 +1,5 @@
 import re
+from time import sleep
 
 import pytest
 
@@ -7,7 +8,6 @@ from tests.e2e.test_e2e_utils import (
     Status,
     assert_job_state,
     wait_job_change_state_from,
-    wait_job_change_state_to,
 )
 from tests.e2e.utils import FILE_SIZE_B, UBUNTU_IMAGE_NAME, format_list
 
@@ -168,40 +168,49 @@ def test_job_storage_interaction(
     # Upload local file
     check_upload_file_to_storage("foo", "data", str(srcfile))
 
-    # Run a job to copy file
-    command = "cp /data/foo /res/foo"
-    captured = run(
-        [
-            "job",
-            "submit",
-            "-m",
-            "20M",
-            "-c",
-            "0.1",
-            "-g",
-            "0",
-            "--http",
-            "80",
-            "--volume",
-            f"{tmpstorage}data:/data:ro",
-            "--volume",
-            f"{tmpstorage}result:/res:rw",
-            "--non-preemptible",
-            UBUNTU_IMAGE_NAME,
-            command,
-        ]
-    )
-    job_id = re.match("Job ID: (.+) Status:", captured.out).group(1)
+    delay = 0.5
+    for i in range(5):
+        # Run a job to copy file
+        command = "cp /data/foo /res/foo"
+        captured = run(
+            [
+                "job",
+                "submit",
+                "-m",
+                "20M",
+                "-c",
+                "0.1",
+                "-g",
+                "0",
+                "--http",
+                "80",
+                "--volume",
+                f"{tmpstorage}data:/data:ro",
+                "--volume",
+                f"{tmpstorage}result:/res:rw",
+                "--non-preemptible",
+                UBUNTU_IMAGE_NAME,
+                command,
+            ]
+        )
+        job_id = re.match("Job ID: (.+) Status:", captured.out).group(1)
 
-    # Wait for job to finish
-    wait_job_change_state_to(run, job_id, Status.SUCCEEDED)
+        # Wait for job to finish
+        wait_job_change_state_from(run, job_id, Status.PENDING)
+        wait_job_change_state_from(run, job_id, Status.RUNNING)
+        try:
+            assert_job_state(run, job_id, Status.SUCCEEDED)
+            # Confirm file has been copied
+            captured = run(["store", "ls", f"{tmpstorage}result"])
+            captured_output_list = captured.out.split("\n")
+            assert not captured.err
+            expected_line = format_list(type="file", size=FILE_SIZE_B, name="foo")
+            assert expected_line in captured_output_list
 
-    # Confirm file has been copied
-    captured = run(["store", "ls", f"{tmpstorage}result"])
-    captured_output_list = captured.out.split("\n")
-    assert not captured.err
-    expected_line = format_list(type="file", size=FILE_SIZE_B, name="foo")
-    assert expected_line in captured_output_list
+            # Download into local dir and confirm checksum
+            check_file_on_storage_checksum("foo", "result", checksum, tmp_path, "bar")
 
-    # Download into local dir and confirm checksum
-    check_file_on_storage_checksum("foo", "result", checksum, tmp_path, "bar")
+            break
+        except AssertionError:
+            sleep(delay)
+            delay *= 2
