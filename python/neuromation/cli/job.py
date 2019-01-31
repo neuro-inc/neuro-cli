b913b0f0a357ca397d2c354d01af205ddc0c10e2
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-01-31 17:06:15 -0500

    Reformat

--- python/neuromation/cli/job.py
+++ python/neuromation/cli/job.py
@@ -92,7 +92,8 @@ def job() -> None:
     "-q", "--quiet", is_flag=True, help="Run command in quiet mode (print only job id)"
 )
 @click.option(
-    "-v", "--volume",
+    "-v",
+    "--volume",
     metavar="MOUNT",
     multiple=True,
     help="Mounts directory from vault into container. "
