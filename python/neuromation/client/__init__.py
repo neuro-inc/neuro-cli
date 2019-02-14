962b2bfb29f2b4571f3125fbb1a5a8f82d4279d4
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-02-14 16:56:54 +0300

    Remove unused method

--- python/neuromation/client/__init__.py
+++ python/neuromation/client/__init__.py
@@ -4,7 +4,6 @@ from typing import Union, Type, Optional
 import aiohttp
 from yarl import URL
 
-from neuromation.client.utils import create_registry_url
 from .abc import AbstractProgress, AbstractSpinner
 from .api import (
     API,
@@ -74,7 +73,7 @@ class Client:
             url = URL(url)
         self._url = url
         # this is temporary until we implement getting server configuration dynamically:
-        registry_url = registry_url or create_registry_url(str(self._url))
+        assert registry_url, "registry url was not provided"
         self._registry_url = URL(registry_url)
         assert token
         self._config = Config(url, self._registry_url, token)
