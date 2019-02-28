32dd83c8a896b4638c0e3a7d9767409b1cd243c6
Ilya Gruzinov <shagren@gmail.com>
2019-02-28 19:33:01 +0300

    Small code cleanup (#573)

--- python/neuromation/client/jobs.py
+++ python/neuromation/client/jobs.py
@@ -111,8 +111,8 @@ class Volume:
         resp: Dict[str, Any] = {
             "src_storage_uri": self.storage_path,
             "dst_path": self.container_path,
+            "read_only": bool(self.read_only),
         }
-        resp["read_only"] = bool(self.read_only)
         return resp
 
     @classmethod
@@ -173,7 +173,7 @@ class HTTPPort:
         return ret
 
     @classmethod
-    def from_api(self, data: Dict[str, Any]) -> "HTTPPort":
+    def from_api(cls, data: Dict[str, Any]) -> "HTTPPort":
         return HTTPPort(**data)
 
 
@@ -186,7 +186,7 @@ class SSHPort:
         return ret
 
     @classmethod
-    def from_api(self, data: Dict[str, Any]) -> "SSHPort":
+    def from_api(cls, data: Dict[str, Any]) -> "SSHPort":
         return SSHPort(**data)
 
 
