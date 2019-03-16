0f9b8be67c6bbfea1a6c959d1e6eb9dc28e68c70
Ilya Gruzinov <shagren@gmail.com>
2019-03-15 22:53:04 -0700

    Better windows ansi escape codes +  colorama supports (#640)
    Better progress reporting for job submitting under windows

--- python/neuromation/cli/formatters/jobs.py
+++ python/neuromation/cli/formatters/jobs.py
@@ -4,6 +4,7 @@ import itertools
 import time
 from dataclasses import dataclass
 from math import floor
+from sys import platform
 from typing import Iterable, Iterator, List, Mapping
 
 import humanize
@@ -17,7 +18,7 @@ from neuromation.client.parsing_utils import ImageNameParser
 BEFORE_PROGRESS = "\r"
 AFTER_PROGRESS = "\n"
 CLEAR_LINE_TAIL = "\033[0K"
-LINE_UP = "\033[1F"
+LINE_UP = "\033[1A"
 
 COLORS = {
     JobStatus.PENDING: "yellow",
@@ -289,7 +290,10 @@ class ResourcesFormatter:
 
 
 class JobStartProgress:
-    SPINNER = ("◢", "◣", "◤", "◥")
+    if platform == "win32":
+        SPINNER = ("-", "\\", "|", "/")
+    else:
+        SPINNER = ("◢", "◣", "◤", "◥")
     LINE_PRE = BEFORE_PROGRESS + "\r" + style("Status", bold=True) + ": "
 
     def __init__(self, color: bool) -> None:
