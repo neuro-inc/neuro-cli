08524ee0bab4489dee811fa65683ba66c32d72a4
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-03-20 16:08:00 +0200

    Fix test

--- python/tests/e2e/test_e2e_jobs.py
+++ python/tests/e2e/test_e2e_jobs.py
@@ -1033,7 +1033,7 @@ def test_job_run(helper):
         [
             "job",
             "run",
-            "-p",
+            "-s",
             "cpu-small",
             "--non-preemptible",
             "--no-wait-start",
