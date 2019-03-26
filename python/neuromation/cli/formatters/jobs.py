44c5f1e7e49e9945c9991b296edb40926e19c2a5
Artem Juszkowski <ajuszkowski@ya.ru>
2019-03-26 15:05:41 +0100

    review: don't print job_alias in shortcuts

--- python/neuromation/cli/formatters/jobs.py
+++ python/neuromation/cli/formatters/jobs.py
@@ -50,24 +50,21 @@ class JobFormatter:
         )
         if job.name:
             out.append(style("Job name", bold=True) + f": {job.name}")
-            job_alias = job.name
-        else:
-            job_alias = job.id
         if job.http_url:
             out.append(style("Http URL", bold=True) + f": {job.http_url}")
         out.append(style("Shortcuts", bold=True) + ":")
 
         out.append(
-            f"  neuro status {job_alias}  " + style("# check job status", dim=True)
+            f"  neuro status {job_id}  " + style("# check job status", dim=True)
         )
         out.append(
-            f"  neuro logs {job_alias}    " + style("# monitor job stdout", dim=True)
+            f"  neuro logs {job_id}    " + style("# monitor job stdout", dim=True)
         )
         out.append(
-            f"  neuro top {job_alias}     "
+            f"  neuro top {job_id}     "
             + style("# display real-time job telemetry", dim=True)
         )
-        out.append(f"  neuro kill {job_alias}    " + style("# kill job", dim=True))
+        out.append(f"  neuro kill {job_id}    " + style("# kill job", dim=True))
         return "\n".join(out)
 
 
