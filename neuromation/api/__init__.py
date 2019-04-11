856a7fab0bd8cb0f1ac4595e22d72589163057a3
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-04-11 17:52:06 +0300

    Implement async context manager

--- neuromation/api/__init__.py
+++ neuromation/api/__init__.py
@@ -1,4 +1,14 @@
 from pathlib import Path
+from types import TracebackType
+from typing import (
+    Any,
+    AsyncContextManager,
+    Awaitable,
+    Coroutine,
+    Generator,
+    Optional,
+    Type,
+)
 
 from yarl import URL
 
@@ -71,17 +81,56 @@ __all__ = (
 _DEFAULT_NMRC_PATH = Path("~/.nmrc")
 
 
-async def get(*, path: Path = _DEFAULT_NMRC_PATH) -> Client:
+class _ContextManager(Awaitable[Client], AsyncContextManager[Client]):
+
+    __slots__ = ("_func", "_client")
+
+    def __init__(self, coro: Coroutine[Any, Any, Client]) -> None:
+        self._coro = coro
+        self._client: Optional[Client] = None
+
+    def __await__(self) -> Generator[Any, None, Client]:
+        return self._coro.__await__()
+
+    async def __aenter__(self) -> Client:
+        self._client = await self._coro
+        assert self._client is not None
+        return self._client
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc: Optional[BaseException],
+        tb: Optional[TracebackType],
+    ) -> Optional[bool]:
+        assert self._client is not None
+        await self._client.close()
+        return None
+
+
+def get(*, path: Path = _DEFAULT_NMRC_PATH) -> _ContextManager:
+    return _ContextManager(_get(path))
+
+
+async def _get(path: Path) -> Client:
     return await Factory(path).get()
 
 
-async def login(url: URL, *, path: Path = _DEFAULT_NMRC_PATH) -> Client:
+def login(url: URL, *, path: Path = _DEFAULT_NMRC_PATH) -> _ContextManager:
+    return _ContextManager(_login(url, path))
+
+
+async def _login(url: URL, path: Path) -> Client:
     return await Factory(path).login(url)
 
 
-async def login_with_token(
+def login_with_token(
     url: URL, token: str, *, path: Path = _DEFAULT_NMRC_PATH
-) -> Client:
+) -> _ContextManager:
+    return _ContextManager(_login_with_token(url, token, path))
+
+
+async def _login_with_token(url: URL, token: str, path: Path) -> Client:
     return await Factory(path).login_with_token(url, token)
 
 
