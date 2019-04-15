3e034f4602b30e6ca2839920655f7e131b3ac49a
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-04-15 16:16:31 +0300

    Reformat

--- tests/e2e/test_e2e_images.py
+++ tests/e2e/test_e2e_images.py
@@ -8,7 +8,7 @@ import aiodocker
 import pytest
 from yarl import URL
 
-from neuromation.api import JobStatus
+from neuromation.api import CONFIG_ENV_NAME, JobStatus
 from tests.e2e.utils import JOB_TINY_CONTAINER_PARAMS
 
 
