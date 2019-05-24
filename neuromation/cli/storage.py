5bb4ab4e935973b198f38d63b2ba3d45679fde62
Serhiy Storchaka <storchaka@gmail.com>
2019-05-24 12:09:04 +0300

    Support variable number of arguments in the storage commands. (#785)

--- neuromation/cli/storage.py
+++ neuromation/cli/storage.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List
 
 import click
 
@@ -27,7 +28,7 @@ def storage() -> None:
 
 
 @command()
-@click.argument("path")
+@click.argument("paths", nargs=-1, required=True)
 @click.option(
     "--recursive",
     "-r",
@@ -35,7 +36,7 @@ def storage() -> None:
     help="remove directories and their contents recursively",
 )
 @async_cmd()
-async def rm(root: Root, path: str, recursive: bool) -> None:
+async def rm(root: Root, paths: List[str], recursive: bool) -> None:
     """
     Remove files or directories.
 
@@ -46,14 +47,15 @@ async def rm(root: Root, path: str, recursive: bool) -> None:
     neuro rm storage://{username}/foo/bar
     neuro rm --recursive storage://{username}/foo/
     """
-    uri = uri_from_cli(path, root.username)
-    log.info(f"Using path '{uri}'")
+    for path in paths:
+        uri = uri_from_cli(path, root.username)
+        log.info(f"Using path '{uri}'")
 
-    await root.client.storage.rm(uri, recursive=recursive)
+        await root.client.storage.rm(uri, recursive=recursive)
 
 
 @command()
-@click.argument("path", default="storage://~")
+@click.argument("paths", nargs=-1)
 @click.option(
     "--human-readable",
     "-h",
@@ -69,49 +71,52 @@ async def rm(root: Root, path: str, recursive: bool) -> None:
 )
 @async_cmd()
 async def ls(
-    root: Root, path: str, human_readable: bool, format_long: bool, sort: str
+    root: Root, paths: List[str], human_readable: bool, format_long: bool, sort: str
 ) -> None:
     """
     List directory contents.
 
     By default PATH is equal user`s home dir (storage:)
     """
-    if format_long:
-        formatter: BaseFilesFormatter = LongFilesFormatter(
-            human_readable=human_readable, color=root.color
-        )
-    else:
-        if root.tty:
-            formatter = VerticalColumnsFilesFormatter(
-                width=root.terminal_size[0], color=root.color
+    if not paths:
+        paths = ["storage://~"]
+    for path in paths:
+        if format_long:
+            formatter: BaseFilesFormatter = LongFilesFormatter(
+                human_readable=human_readable, color=root.color
             )
         else:
-            formatter = SimpleFilesFormatter(root.color)
+            if root.tty:
+                formatter = VerticalColumnsFilesFormatter(
+                    width=root.terminal_size[0], color=root.color
+                )
+            else:
+                formatter = SimpleFilesFormatter(root.color)
 
-    uri = uri_from_cli(path, root.username)
-    log.info(f"Using path '{uri}'")
+        uri = uri_from_cli(path, root.username)
+        log.info(f"Using path '{uri}'")
 
-    files = await root.client.storage.ls(uri)
+        files = await root.client.storage.ls(uri)
 
-    files = sorted(files, key=FilesSorter(sort).key())
+        files = sorted(files, key=FilesSorter(sort).key())
 
-    for line in formatter.__call__(files):
-        click.echo(line)
+        for line in formatter.__call__(files):
+            click.echo(line)
 
 
 @command()
-@click.argument("source")
+@click.argument("sources", nargs=-1, required=True)
 @click.argument("destination")
 @click.option("-r", "--recursive", is_flag=True, help="Recursive copy, off by default")
 @click.option("-p", "--progress", is_flag=True, help="Show progress, off by default")
 @async_cmd()
 async def cp(
-    root: Root, source: str, destination: str, recursive: bool, progress: bool
+    root: Root, sources: List[str], destination: str, recursive: bool, progress: bool
 ) -> None:
     """
     Copy files and directories.
 
-    Either SOURCE or DESTINATION should have storage:// scheme.
+    Either SOURCES or DESTINATION should have storage:// scheme.
     If scheme is omitted, file:// scheme is assumed.
 
     Examples:
@@ -124,35 +129,36 @@ async def cp(
     # explicit file:// scheme set
     neuro cp storage:///foo file:///foo
     """
-    src = uri_from_cli(source, root.username)
     dst = uri_from_cli(destination, root.username)
+    log.info(f"Using destination path: '{dst}'")
 
-    progress_obj = ProgressBase.create_progress(progress)
+    for source in sources:
+        src = uri_from_cli(source, root.username)
 
-    if src.scheme == "file" and dst.scheme == "storage":
-        log.info(f"Using source path:      '{src}'")
-        log.info(f"Using destination path: '{dst}'")
-        if recursive:
-            await root.client.storage.upload_dir(src, dst, progress=progress_obj)
+        progress_obj = ProgressBase.create_progress(progress)
+
+        if src.scheme == "file" and dst.scheme == "storage":
+            log.info(f"Using source path:      '{src}'")
+            if recursive:
+                await root.client.storage.upload_dir(src, dst, progress=progress_obj)
+            else:
+                await root.client.storage.upload_file(src, dst, progress=progress_obj)
+        elif src.scheme == "storage" and dst.scheme == "file":
+            log.info(f"Using source path:      '{src}'")
+            if recursive:
+                await root.client.storage.download_dir(src, dst, progress=progress_obj)
+            else:
+                await root.client.storage.download_file(src, dst, progress=progress_obj)
         else:
-            await root.client.storage.upload_file(src, dst, progress=progress_obj)
-    elif src.scheme == "storage" and dst.scheme == "file":
-        log.info(f"Using source path:      '{src}'")
-        log.info(f"Using destination path: '{dst}'")
-        if recursive:
-            await root.client.storage.download_dir(src, dst, progress=progress_obj)
-        else:
-            await root.client.storage.download_file(src, dst, progress=progress_obj)
-    else:
-        raise RuntimeError(
-            f"Copy operation of the file with scheme '{src.scheme}'"
-            f" to the file with scheme '{dst.scheme}'"
-            f" is not supported"
-        )
+            raise RuntimeError(
+                f"Copy operation of the file with scheme '{src.scheme}'"
+                f" to the file with scheme '{dst.scheme}'"
+                f" is not supported"
+            )
 
 
 @command()
-@click.argument("path")
+@click.argument("paths", nargs=-1, required=True)
 @click.option(
     "-p",
     "--parents",
@@ -160,22 +166,23 @@ async def cp(
     help="No error if existing, make parent directories as needed",
 )
 @async_cmd()
-async def mkdir(root: Root, path: str, parents: bool) -> None:
+async def mkdir(root: Root, paths: List[str], parents: bool) -> None:
     """
     Make directories.
     """
 
-    uri = uri_from_cli(path, root.username)
-    log.info(f"Using path '{uri}'")
+    for path in paths:
+        uri = uri_from_cli(path, root.username)
+        log.info(f"Using path '{uri}'")
 
-    await root.client.storage.mkdirs(uri, parents=parents, exist_ok=parents)
+        await root.client.storage.mkdirs(uri, parents=parents, exist_ok=parents)
 
 
 @command()
-@click.argument("source")
+@click.argument("sources", nargs=-1, required=True)
 @click.argument("destination")
 @async_cmd()
-async def mv(root: Root, source: str, destination: str) -> None:
+async def mv(root: Root, sources: List[str], destination: str) -> None:
     """
     Move or rename files and directories.
 
@@ -195,12 +202,13 @@ async def mv(root: Root, source: str, destination: str) -> None:
     neuro mv storage://{username}/foo/ storage://{username}/bar/baz/foo/
     """
 
-    src = uri_from_cli(source, root.username)
     dst = uri_from_cli(destination, root.username)
-    log.info(f"Using source path:      '{src}'")
     log.info(f"Using destination path: '{dst}'")
+    for source in sources:
+        src = uri_from_cli(source, root.username)
+        log.info(f"Using source path:      '{src}'")
 
-    await root.client.storage.mv(src, dst)
+        await root.client.storage.mv(src, dst)
 
 
 storage.add_command(cp)
