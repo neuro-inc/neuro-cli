48186ce828d02a5dc3c1438f0aba177f6442a8d7
Serhiy Storchaka <storchaka@gmail.com>
2019-05-28 14:50:54 +0300

    Use a workaround for the Path.absolute() bug.

--- tests/api/test_url_utils.py
+++ tests/api/test_url_utils.py
@@ -262,14 +262,14 @@ async def test_normalize_storage_path_uri__tilde_in_relative_path_3(
 
 
 async def test_normalize_local_path_uri__tilde_in_relative_path_3(
-    fake_homedir: Path
+    fake_homedir: Path, pwd: Path
 ) -> None:
     url = URL("file:path/to~file.txt")
     url = normalize_local_path_uri(url)
     assert url.scheme == "file"
     assert url.host is None
-    assert _extract_path(url) == Path("path/to~file.txt").resolve()
-    assert str(url) == Path("path/to~file.txt").resolve().as_uri().replace("%7E", "~")
+    assert _extract_path(url) == pwd / "path/to~file.txt"
+    assert str(url) == (pwd / "path/to~file.txt").as_uri().replace("%7E", "~")
 
 
 async def test_normalize_storage_path_uri__tilde_in_absolute_path(
@@ -284,16 +284,14 @@ async def test_normalize_storage_path_uri__tilde_in_absolute_path(
 
 
 async def test_normalize_local_path_uri__tilde_in_absolute_path(
-    fake_homedir: Path
+    fake_homedir: Path, pwd: Path
 ) -> None:
     url = URL("file:/~/path/to/file.txt")
     url = normalize_local_path_uri(url)
     assert url.scheme == "file"
     assert url.host is None
-    assert _extract_path(url) == Path("/~/path/to/file.txt").resolve()
-    assert str(url) == Path("/~/path/to/file.txt").resolve().as_uri().replace(
-        "%7E", "~"
-    )
+    assert _extract_path(url) == pwd / "/~/path/to/file.txt"
+    assert str(url) == (pwd / "/~/path/to/file.txt").as_uri().replace("%7E", "~")
 
 
 async def test_normalize_storage_path_uri__tilde_in_host(client: Client) -> None:
