4e284a3271d8be6e526ccf68c9073775e64446ab
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-06-04 16:30:32 +0300

    Revert back keys sorting in nmrc to support older yaml versions

--- neuromation/api/config_factory.py
+++ neuromation/api/config_factory.py
@@ -299,7 +299,7 @@ class Factory:
             with open(  # type: ignore
                 tmppath, "x", encoding="utf-8", opener=opener
             ) as f:
-                yaml.safe_dump(payload, f, default_flow_style=False, sort_keys=False)
+                yaml.safe_dump(payload, f, default_flow_style=False)
             os.replace(tmppath, self._path)
         except:  # noqa  # bare 'except' with 'raise' is legal
             try:
