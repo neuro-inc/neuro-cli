e68d028b351ee7cc28ca402d02c07d6b4f876d03
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-06-05 13:19:24 +0300

    Relax yaml requirement (#828)
    * Revert back keys sorting in nmrc to support older yaml versions

--- neuromation/api/config_factory.py
+++ neuromation/api/config_factory.py
@@ -4,7 +4,7 @@ import ssl
 import sys
 from dataclasses import replace
 from pathlib import Path
-from typing import Any, Awaitable, Callable, Dict, Optional
+from typing import Any, Awaitable, Callable, Dict, Optional, Tuple
 
 import aiohttp
 import certifi
@@ -212,14 +212,17 @@ class Factory:
             "storage_url": str(cluster_config.storage_url),
             "users_url": str(cluster_config.users_url),
             "monitoring_url": str(cluster_config.monitoring_url),
-            "resource_presets": {
-                name: self._serialize_resource_preset(resource_preset)
+            "resource_presets": [
+                self._serialize_resource_preset(name, resource_preset)
                 for name, resource_preset in cluster_config.resource_presets.items()
-            },
+            ],
         }
 
-    def _serialize_resource_preset(self, resource_preset: RunPreset) -> Dict[str, Any]:
+    def _serialize_resource_preset(
+        self, name: str, resource_preset: RunPreset
+    ) -> Dict[str, Any]:
         return {
+            "name": name,
             "cpu": resource_preset.cpu,
             "memory_mb": resource_preset.memory_mb,
             "gpu": resource_preset.gpu,
@@ -248,18 +251,23 @@ class Factory:
             storage_url=URL(cluster_config["storage_url"]),
             users_url=URL(cluster_config["users_url"]),
             monitoring_url=URL(cluster_config["monitoring_url"]),
-            resource_presets={
-                name: self._deserialize_resource_preset(data)
-                for name, data in cluster_config.get("resource_presets", {}).items()
-            },
+            resource_presets=dict(
+                self._deserialize_resource_preset(data)
+                for data in cluster_config.get("resource_presets", [])
+            ),
         )
 
-    def _deserialize_resource_preset(self, payload: Dict[str, Any]) -> RunPreset:
-        return RunPreset(
-            cpu=payload["cpu"],
-            memory_mb=payload["memory_mb"],
-            gpu=payload.get("gpu"),
-            gpu_model=payload.get("gpu_model"),
+    def _deserialize_resource_preset(
+        self, payload: Dict[str, Any]
+    ) -> Tuple[str, RunPreset]:
+        return (
+            payload["name"],
+            RunPreset(
+                cpu=payload["cpu"],
+                memory_mb=payload["memory_mb"],
+                gpu=payload.get("gpu"),
+                gpu_model=payload.get("gpu_model"),
+            ),
         )
 
     def _deserialize_auth_token(self, payload: Dict[str, Any]) -> _AuthToken:
@@ -299,7 +307,7 @@ class Factory:
             with open(  # type: ignore
                 tmppath, "x", encoding="utf-8", opener=opener
             ) as f:
-                yaml.safe_dump(payload, f, default_flow_style=False, sort_keys=False)
+                yaml.safe_dump(payload, f, default_flow_style=False)
             os.replace(tmppath, self._path)
         except:  # noqa  # bare 'except' with 'raise' is legal
             try:
