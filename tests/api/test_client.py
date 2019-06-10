3452b670ecb3c172ec82d2944dc986da96048d8e
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-06-10 14:12:32 +0300

    Add a test

--- tests/api/test_client.py
+++ tests/api/test_client.py
@@ -9,3 +9,9 @@ _MakeClient = Callable[..., Client]
 async def test_client_username(make_client: _MakeClient) -> None:
     async with make_client("http://example.com") as client:
         assert client.username == "user"
+
+
+
+async def test_client__get_session_cookie(make_client: _MakeClient) -> None:
+    async with make_client("http://example.com") as client:
+        assert client._get_session_cookie() is None
