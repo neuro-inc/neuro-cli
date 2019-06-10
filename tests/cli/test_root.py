4fadb647fcad118bb1287b566af4f7bd7ceb96ff
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-06-10 14:15:38 +0300

    Fix linter

--- tests/cli/test_root.py
+++ tests/cli/test_root.py
@@ -7,7 +7,7 @@ from neuromation.cli.root import ConfigError, Root
 
 
 @pytest.fixture
-def root_uninitialized():
+def root_uninitialized() -> Root:
     return Root(
         color=False,
         tty=False,
@@ -18,28 +18,28 @@ def root_uninitialized():
     )
 
 
-def test_auth_uninitialized(root_uninitialized):
+def test_auth_uninitialized(root_uninitialized: Root) -> None:
     assert root_uninitialized.auth is None
 
 
-def test_timeout(root_uninitialized):
+def test_timeout(root_uninitialized: Root) -> None:
     assert root_uninitialized.timeout == aiohttp.ClientTimeout(None, None, 60, 60)
 
 
-def test_username_uninitialized(root_uninitialized):
+def test_username_uninitialized(root_uninitialized: Root) -> None:
     with pytest.raises(ConfigError):
         root_uninitialized.username
 
 
-def test_url_uninitialized(root_uninitialized):
+def test_url_uninitialized(root_uninitialized: Root) -> None:
     with pytest.raises(ConfigError):
         root_uninitialized.url
 
 
-def test_resource_presets_uninitialized(root_uninitialized):
+def test_resource_presets_uninitialized(root_uninitialized: Root) -> None:
     with pytest.raises(ConfigError):
         root_uninitialized.resource_presets
 
 
-def test_get_session_cookie(root_uninitialized):
+def test_get_session_cookie(root_uninitialized: Root) -> None:
     assert root_uninitialized.get_session_cookie() is None
