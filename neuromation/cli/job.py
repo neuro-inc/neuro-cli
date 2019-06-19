4f477e4c44c37b4fe2a2430a595e88e64cead015
Alexey Naiden <alexey.nayden@gmail.com>
2019-06-19 16:50:55 +0300

    fix attempt

--- neuromation/cli/job.py
+++ neuromation/cli/job.py
@@ -729,7 +729,9 @@ async def run_job(
         local_nmrc_folder = "/var/storage/nmrc/"
         local_nmrc_path = f"{local_nmrc_folder}{random_nmrc_filename}"
 
-        await root.client.storage.mkdirs(storage_nmrc_path, parents=True, exist_ok=True)
+        await root.client.storage.mkdirs(
+            URL(storage_nmrc_folder), parents=True, exist_ok=True
+        )
         await root.client.storage.upload_file(nmrc_path, storage_nmrc_path)
 
         # specify a container volume and mount the storage path
