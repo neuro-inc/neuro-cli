f7370edb671ac6c4f92355792292390c8baeff2a
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-07-15 15:04:21 +0300

    Drop file: prefix in progress reporting

--- neuromation/cli/command_progress_report.py
+++ neuromation/cli/command_progress_report.py
@@ -4,23 +4,37 @@ import click
 from yarl import URL
 
 from neuromation.api import AbstractStorageProgress
+from neuromation.api.url_utils import _extract_path
 
 
 class ProgressBase(AbstractStorageProgress):
     def start(self, src: URL, dst: URL, size: int) -> None:
         pass
 
-    def complete(self, src: URL, dst: URL) -> None:
+    def complete(self, src_url: URL, dst_url: URL) -> None:
+        src = self.fmt_url(src_url)
+        dst = self.fmt_url(dst_url)
         click.echo(f"{src!r} -> {dst!r}")
 
     def progress(self, src: URL, dst: URL, current: int) -> None:
         pass
 
-    def mkdir(self, src: URL, dst: URL) -> None:
+    def mkdir(self, src_url: URL, dst_url: URL) -> None:
+        src = self.fmt_url(src_url)
+        dst = self.fmt_url(dst_url)
         click.echo(f"{src!r} -> {dst!r}")
 
-    def fail(self, src: URL, dst: URL, message: str) -> None:  # pragma: no cover
+    def fail(self, src_url: URL, dst_url: URL, message: str) -> None:
+        src = self.fmt_url(src_url)
+        dst = self.fmt_url(dst_url)
         click.echo(f"Failure: {src:!} -> {dst!r} [{message}]", err=True)
+
+    def fmt_url(self, url: URL) -> str:
+        if url.scheme == "file":
+            path = _extract_path(url)
+            return str(path)
+        else:
+            return str(url)
 
     @classmethod
     def create_progress(
@@ -35,32 +49,36 @@ class ProgressBase(AbstractStorageProgress):
 
 class StandardPrintPercentOnly(ProgressBase):
     def __init__(self) -> None:
-        self._src: Optional[URL] = None
-        self._dst: Optional[URL] = None
+        self._src: Optional[str] = None
+        self._dst: Optional[str] = None
         self._file_size: Optional[int] = None
 
     def start(self, src: URL, dst: URL, size: int) -> None:
-        self._src = src
-        self._dst = dst
+        self._src = self.fmt_url(src)
+        self._dst = self.fmt_url(dst)
         self._file_size = size
         click.echo(f"Start copying {self._src!r} -> {self._dst!r}.")
 
     def complete(self, src: URL, dst: URL) -> None:
-        self._src = src
-        self._dst = dst
+        self._src = self.fmt_url(src)
+        self._dst = self.fmt_url(dst)
         click.echo(f"\rFile {self._src!r} -> {self._dst!r} copying completed.")
 
     def progress(self, src: URL, dst: URL, current: int) -> None:
-        self._src = src
-        self._dst = dst
+        self._src = self.fmt_url(src)
+        self._dst = self.fmt_url(dst)
         if self._file_size:
             progress = (100 * current) / self._file_size
         else:
             progress = 0
         click.echo(f"\r{self._src!r} -> {self._dst!r}: {progress:.2f}%.", nl=False)
 
-    def mkdir(self, src: URL, dst: URL) -> None:
+    def mkdir(self, src_url: URL, dst_url: URL) -> None:
+        src = self.fmt_url(src_url)
+        dst = self.fmt_url(dst_url)
         click.echo(f"Copy directory {src!r} -> {dst!r}.")
 
-    def fail(self, src: URL, dst: URL, message: str) -> None:  # pragma: no cover
+    def fail(self, src_url: URL, dst_url: URL, message: str) -> None:
+        src = self.fmt_url(src_url)
+        dst = self.fmt_url(dst_url)
         click.echo(f"Failure: {src:!} -> {dst!r} [{message}]", err=True)
--- tests/cli/test_progress_report.py
+++ tests/cli/test_progress_report.py
@@ -26,30 +26,34 @@ def test_progress_factory_percent() -> None:
 def test_simple_progress(capsys: Any) -> None:
     report = StandardPrintPercentOnly()
     src = URL("file:///abc")
+    src_str = "/abc"
     dst = URL("storage:xyz")
+    dst_str = "storage:xyz"
 
     report.start(src, dst, 600)
     captured = capsys.readouterr()
-    assert captured.out == f"Start copying {src!r} -> {dst!r}.\n"
+    assert captured.out == f"Start copying {src_str!r} -> {dst_str!r}.\n"
 
     report.progress(src, dst, 300)
     captured = capsys.readouterr()
-    assert captured.out == f"\r{src!r} -> {dst!r}: 50.00%."
+    assert captured.out == f"\r{src_str!r} -> {dst_str!r}: 50.00%."
 
     report.progress(src, dst, 400)
     captured = capsys.readouterr()
-    assert captured.out == f"\r{src!r} -> {dst!r}: 66.67%."
+    assert captured.out == f"\r{src_str!r} -> {dst_str!r}: 66.67%."
 
     report.complete(src, dst)
     captured = capsys.readouterr()
-    assert captured.out == f"\rFile {src!r} -> {dst!r} copying completed.\n"
+    assert captured.out == f"\rFile {src_str!r} -> {dst_str!r} copying completed.\n"
 
 
 def test_mkdir(capsys: Any) -> None:
     report = StandardPrintPercentOnly()
     src = URL("file:///abc")
+    src_str = '/abc'
     dst = URL("storage:xyz")
+    dst_str = "storage:xyz"
 
     report.mkdir(src, dst)
     captured = capsys.readouterr()
-    assert captured.out == f"Copy directory {src!r} -> {dst!r}.\n"
+    assert captured.out == f"Copy directory {src_str!r} -> {dst_str!r}.\n"
