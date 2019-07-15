f083a3688e8daa105c29245c01a610ee78f90a26
Alexey Naiden <alexey.nayden@gmail.com>
2019-07-15 13:35:55 +0300

    Fix a test

--- tests/e2e/test_e2e_images.py
+++ tests/e2e/test_e2e_images.py
@@ -119,6 +119,7 @@ def test_images_complete_lifecycle(
             "0",
             "--non-preemptible",
             "--no-wait-start",
+            "--detach",
         ]
     )
     assert not captured.err
