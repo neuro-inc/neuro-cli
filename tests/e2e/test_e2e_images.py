e8b05a551800a5be5744390f10f1946a11b0a0a2
Aleksandr Danshyn <dalazx@gmail.com>
2019-07-18 21:39:06 +0300

    Some debug info

--- tests/e2e/test_e2e_images.py
+++ tests/e2e/test_e2e_images.py
@@ -47,9 +47,10 @@ async def generate_image(docker: aiodocker.Docker, tag: str) -> str:
     # TODO use random image name here
     image_name = f"{TEST_IMAGE_NAME}:{tag}"
     with image_archive.open(mode="r+b") as fileobj:
-        await docker.images.build(
+        result = await docker.images.build(
             fileobj=fileobj, tag=image_name, buildargs={"TAG": tag}, encoding="identity"
         )
+        print(result)
 
     return image_name
 
