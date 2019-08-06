fbea81e2ee322751ed617503364e2d3cd7393001
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-08-06 15:13:06 +0300

    Make Client.closing() idempotent

--- tests/api/test_client.py
+++ tests/api/test_client.py
@@ -14,3 +14,12 @@ async def test_client_username(make_client: _MakeClient) -> None:
 async def test_client__get_session_cookie(make_client: _MakeClient) -> None:
     async with make_client("http://example.com") as client:
         assert client._get_session_cookie() is None
+
+
+async def test_client_double_closing(make_client: _MakeClient) -> None:
+    client = make_client("http://example.com")
+    assert not client._closing
+    await client.close()
+    assert client._closing
+    await client.close()
+    assert client._closing
