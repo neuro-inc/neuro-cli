b55d41995511a51eb60a9e6ae7952f1f04c9f643
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-09-02 15:44:49 +0300

    Threaded waiter (#1005)

--- neuromation/cli/asyncio_utils.py
+++ neuromation/cli/asyncio_utils.py
@@ -1,14 +1,22 @@
 import asyncio
 import gc
+import itertools
+import logging
+import os
 import sys
+import threading
 import warnings
-from typing import Awaitable, TypeVar
+from types import TracebackType
+from typing import Any, Awaitable, Callable, Dict, List, Optional, Type, TypeVar
 
 
 _T = TypeVar("_T")
+logger = logging.getLogger(__name__)
 
 
 def run(main: Awaitable[_T], *, debug: bool = False) -> _T:
+    # Backport from python 3.7
+
     """Run a coroutine.
 
     This function runs the passed coroutine, taking care of
@@ -99,3 +107,119 @@ def _cancel_all_tasks(
                     "task": task,
                 }
             )
+
+
+if sys.platform != "win32":
+    from asyncio.unix_events import AbstractChildWatcher  # type: ignore
+
+    _Callback = Callable[..., None]
+
+    class ThreadedChildWatcher(AbstractChildWatcher):
+        # Backport from Python 3.8
+
+        """Threaded child watcher implementation.
+
+        The watcher uses a thread per process
+        for waiting for the process finish.
+
+        It doesn't require subscription on POSIX signal
+        but a thread creation is not free.
+
+        The watcher has O(1) complexity, its performance doesn't depend
+        on amount of spawn processes.
+        """
+
+        def __init__(self) -> None:
+            self._pid_counter = itertools.count(0)
+            self._threads: Dict[int, threading.Thread] = {}
+
+        def close(self) -> None:
+            pass
+
+        def __enter__(self) -> "ThreadedChildWatcher":
+            return self
+
+        def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[TracebackType],
+        ) -> None:
+            pass
+
+        def __del__(self, _warn: Any = warnings.warn) -> None:
+            threads = [
+                thread for thread in list(self._threads.values()) if thread.is_alive()
+            ]
+            if threads:
+                _warn(
+                    f"{self.__class__} has registered but not finished child processes",
+                    ResourceWarning,
+                    source=self,
+                )
+
+        def add_child_handler(self, pid: int, callback: _Callback, *args: Any) -> None:
+            loop = asyncio.get_event_loop()
+            thread = threading.Thread(
+                target=self._do_waitpid,
+                name=f"waitpid-{next(self._pid_counter)}",
+                args=(loop, pid, callback, args),
+                daemon=True,
+            )
+            self._threads[pid] = thread
+            thread.start()
+
+        def remove_child_handler(self, pid: int) -> bool:
+            # asyncio never calls remove_child_handler() !!!
+            # The method is no-op but is implemented because
+            # abstract base classe requires it
+            return True
+
+        def attach_loop(self, loop: asyncio.AbstractEventLoop) -> None:
+            pass
+
+        def _do_waitpid(
+            self,
+            loop: asyncio.AbstractEventLoop,
+            expected_pid: int,
+            callback: _Callback,
+            args: List[Any],
+        ) -> None:
+            assert expected_pid > 0
+
+            try:
+                pid, status = os.waitpid(expected_pid, 0)
+            except ChildProcessError:
+                # The child process is already reaped
+                # (may happen if waitpid() is called elsewhere).
+                pid = expected_pid
+                returncode = 255
+                logger.warning(
+                    "Unknown child process pid %d, will report returncode 255", pid
+                )
+            else:
+                returncode = _compute_returncode(status)
+                if loop.get_debug():
+                    logger.debug(
+                        "process %s exited with returncode %s", expected_pid, returncode
+                    )
+
+            if loop.is_closed():
+                logger.warning("Loop %r that handles pid %r is closed", loop, pid)
+            else:
+                loop.call_soon_threadsafe(callback, pid, returncode, *args)
+
+            self._threads.pop(expected_pid)
+
+    def _compute_returncode(status: int) -> int:
+        if os.WIFSIGNALED(status):
+            # The child process died because of a signal.
+            return -os.WTERMSIG(status)
+        elif os.WIFEXITED(status):
+            # The child process exited (e.g sys.exit()).
+            return os.WEXITSTATUS(status)
+        else:
+            # The child exited, but we don't understand its status.
+            # This shouldn't happen, but if it does, let's just
+            # return that status; perhaps that helps debug it.
+            return status
