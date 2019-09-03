1db4a16cb59c4c185f3d22c0a9e4ff88942873f1
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-09-03 16:18:51 +0300

    drop warning

--- neuromation/cli/utils.py
+++ neuromation/cli/utils.py
@@ -461,11 +461,6 @@ async def resolve_job(
             f"{details} to a job-ID: {e}"
         )
     if jobs:
-        if len(jobs) > 1:
-            log.warning(
-                f"Found {len(jobs)} jobs matching {details}: "
-                ", ".join(job.id for job in jobs)
-            )
         job_id = jobs[-1].id
         log.debug(f"Job name '{id_or_name}' resolved to job ID '{job_id}'")
     else:
