51d48c014598c6e827f91ddecaa895663ca995ec
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-09-07 20:43:34 +0300

    Work on

--- neuromation/cli/autocomplete.py
+++ neuromation/cli/autocomplete.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import os
 import socket
 import struct
@@ -8,6 +9,10 @@ from typing import List, Tuple
 
 import click
 from typing_extensions import TypedDict
+
+
+CONFIG = "autocomplete.json"
+SOCKNAME = "autocomplete.sock"
 
 
 class Request(TypedDict):
@@ -22,6 +27,9 @@ def complete_job(
 
 
 def send_cmd(request: Request) -> List[Tuple[str, str]]:
+    # todo: send calculated config path,
+    # skip autocompletion if the path doesn't match default
+    # to prevent leaking sensitive data
     bin_request = json.dumps(request).encode("uft-8")
     pre = struct.pack("<I", len(bin_request))
     maybe_start_server()
@@ -36,22 +44,52 @@ def send_cmd(request: Request) -> List[Tuple[str, str]]:
 
 
 def maybe_start_server():
+    folder = get_folder()
+    try:
+        # restrict the access to current user only
+        folder.mkdir(0o700)
+    except FileExistsError:
+        pass
+    try:
+        with (folder / CONFIG).open() as f:
+            config = json.load(f)
+            pid = config["pid"]
+            # check if the server is alive
+            os.kill(pid, 0)
+    except (OSError, TypeError, ValueError):
+        # file not found or malformed,
+        # or the server is stopped
+        # restart the server
+        pass
+
+    sockname = folder / SOCKNAME
+    if sockname.exists():
+        sockname.unlink()
+
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.bind(str(sockname))
     pass
 
 
 def getid() -> str:
-    return str(os.getsid())
+    return str(os.getsid(0))
 
 
-def getfolder() -> Path:
+def get_folder() -> Path:
+    if sys.implementation.platform == "linux":
+        # Linux has user-specific non-persistent location
+        tmpdir = Path(f"/var/run/user/{os.getuid()}")
+    else:
+        tmpdir = Path(tempfile.gettempdir())
     sid = getid()
-    ret = Path(tempfile.gettempdir()) / f"neuro-{sid}"
-    try:
-        ret.mkdir(0o700)
-    except FileExistsError:
-        if (ret / "autocomplete.json").exists():
-            pass
+    return tmpdir / f"neuro-{sid}"
 
 
 def get_socket() -> socket.socket:
+    pass
+
+
+def main():
+    """Enrypoint for autocomplete sugesstion server."""
+
     pass
