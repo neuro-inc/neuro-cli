8912c80b4d35903fda3551b90e8fdf6f4c8d3e22
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-12-04 12:24:56 +0200

    Use default cluster name if not specified

--- neuromation/cli/config.py
+++ neuromation/cli/config.py
@@ -250,6 +250,8 @@ async def prompt_cluster(client: Client) -> str:
         click.echo("\n".join(fmt(clusters.values(), client.config.cluster_name)))
         answer = input(f"Select cluster to switch [{client.config.cluster_name}]: ")
         answer = answer.strip()
+        if answer is None:
+            answer = client.config.cluster_name
         if answer not in clusters:
             click.echo(
                 " ".join(
