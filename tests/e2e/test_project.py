b724610ab8ca6955ee8323ea0ecf2c3f14828f47
Alexey Naiden <alexey.nayden@gmail.com>
2019-12-17 17:08:34 +0300

    Add e2e tests

--- tests/e2e/test_project.py
+++ tests/e2e/test_project.py
@@ -1,3 +1,6 @@
+import pathlib
+import shutil
+
 import pytest
 
 from tests.e2e import Helper
@@ -5,5 +8,23 @@ from tests.e2e import Helper
 
 @pytest.mark.e2e
 def test_project_init(helper: Helper) -> None:
-    captured = helper.run_cli(["project", "init"])
-    assert captured.out is None
+    path = pathlib.Path(f"./name-of-the-project")
+    assert not path.exists()
+    try:
+        helper.run_cli(["project", "init", "-q"])
+        assert path.exists()
+        assert path.is_dir()
+    finally:
+        shutil.rmtree(path)
+
+
+@pytest.mark.e2e
+def test_project_init_custom_slug(helper: Helper) -> None:
+    path = pathlib.Path("./my-custom-slug")
+    assert not path.exists()
+    try:
+        helper.run_cli(["project", "init", "-q", "my-custom-slug"])
+        assert path.exists()
+        assert path.is_dir()
+    finally:
+        shutil.rmtree(path)
