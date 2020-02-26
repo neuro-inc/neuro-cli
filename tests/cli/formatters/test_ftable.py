9cd6c97dbbee033392f564ea4eb5be3e509c6b0a
Oleg Stepanov <oleg.stepanov@gmail.com>
2020-02-26 17:32:28 +0300

    Added support for empty input in table()

--- tests/cli/formatters/test_ftable.py
+++ tests/cli/formatters/test_ftable.py
@@ -58,6 +58,10 @@ class TestRow:
 
 
 class TestTable:
+    def test_empty(self) -> None:
+        result = list(table(()))
+        assert len(result) == 0
+
     def test_simple(self) -> None:
         rows = [["a", "Alpha"], ["b", "Bravo"]]
         result = list(
