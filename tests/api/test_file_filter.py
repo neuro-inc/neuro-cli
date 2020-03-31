19c85acd9ce9969230c8679c9ae668bc8af12c15
Serhiy Storchaka <storchaka@gmail.com>
2020-03-31 19:50:15 +0300

    Revert "Make filter patterns more compatible with .gitignore."
    This reverts commit f7672911058494a96c7f092b2ee637b5d0e61ca4.

--- tests/api/test_file_filter.py
+++ tests/api/test_file_filter.py
@@ -1,4 +1,6 @@
-from neuromation.api.file_filter import FileFilter, translate
+import os.path
+
+from neuromation.api.file_filter import FileFilter
 
 
 async def test_empty_filter() -> None:
@@ -6,6 +8,7 @@ async def test_empty_filter() -> None:
     assert await ff.match("spam")
     assert await ff.match(".spam")
     assert await ff.match("spam/ham")
+    assert await ff.match(os.path.join("spam", "ham"))
 
 
 async def test_exclude_all() -> None:
@@ -20,9 +23,6 @@ async def test_exclude() -> None:
     ff.exclude("*.txt")
     assert await ff.match("spam")
     assert not await ff.match("spam.txt")
-    assert not await ff.match("dir/spam.txt")
-    assert await ff.match("dir.txt/spam")
-    assert await ff.match("dir/child.txt/spam")
 
 
 async def test_exclude_include() -> None:
@@ -33,71 +33,3 @@ async def test_exclude_include() -> None:
     assert await ff.match("spam.txt")
     assert await ff.match("ham")
     assert not await ff.match("ham.txt")
-    assert await ff.match("dir/spam.txt")
-    assert not await ff.match("dir/ham.txt")
-    assert await ff.match("dir.txt/spam")
-
-
-async def test_exclude_with_slash() -> None:
-    ff = FileFilter()
-    ff.exclude("dir/*.txt")
-    assert await ff.match("spam.txt")
-    assert not await ff.match("dir/spam.txt")
-    assert await ff.match("parent/dir/spam.txt")
-
-
-async def test_exclude_recursive() -> None:
-    ff = FileFilter()
-    ff.exclude("**/dir/*.txt")
-    assert await ff.match("spam.txt")
-    assert not await ff.match("dir/spam.txt")
-    assert await ff.match("dir/spam")
-    assert not await ff.match("parent/dir/spam.txt")
-    assert await ff.match("parent/dir/spam")
-
-    ff = FileFilter()
-    ff.exclude("dir/**/*.txt")
-    assert await ff.match("spam.txt")
-    assert not await ff.match("dir/spam.txt")
-    assert await ff.match("dir/spam")
-    assert not await ff.match("dir/child/spam.txt")
-    assert await ff.match("dir/child/spam")
-
-    ff = FileFilter()
-    ff.exclude("dir/**")
-    assert await ff.match("spam")
-    assert not await ff.match("dir/")
-    assert await ff.match("dir")
-    assert not await ff.match("dir/child")
-    assert not await ff.match("dir/child/spam")
-
-    ff = FileFilter()
-    ff.exclude("dir/**/")
-    assert await ff.match("spam")
-    assert not await ff.match("dir/")
-    assert await ff.match("dir")
-    assert not await ff.match("dir/child/")
-    assert await ff.match("dir/child")
-    assert not await ff.match("dir/child/spam/")
-    assert await ff.match("dir/child/spam")
-
-
-def test_translate() -> None:
-    assert translate("") == ""
-    assert translate("abc") == "abc"
-    assert translate("a?c") == "a[^/]c"
-    assert translate("a*c") == "a[^/]*c"
-    assert translate("a[bc]d") == "a[bc]d"
-    assert translate("a[b-d]e") == "a[b-d]e"
-    assert translate("a[!b-d]e") == "a[^b-d]e"
-    assert translate("[a-zA-Z_]") == "[a-zA-Z_]"
-
-
-def test_translate_recursive() -> None:
-    assert translate("**") == ".*"
-    assert translate("**/") == f"(?:.+/)?"
-    assert translate("**/abc") == f"(?:.+/)?abc"
-    assert translate("/**") == f"/.*"
-    assert translate("abc/**") == f"abc/.*"
-    assert translate("/**/") == f"/(?:.+/)?"
-    assert translate("abc/**/def") == f"abc/(?:.+/)?def"
