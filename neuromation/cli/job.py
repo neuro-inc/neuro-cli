5a74ffb9c006ccfe4c8c3447a43a9225ad4a7fa9
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-06-19 20:12:43 +0300

    Drop exec timeout functionality, hide --timeout option

--- neuromation/cli/job.py
+++ neuromation/cli/job.py
@@ -409,6 +409,7 @@ async def submit(
     default=0,
     type=float,
     show_default=True,
+    hidden=True,
     help="Maximum allowed time for executing the command, 0 for no timeout",
 )
 async def exec(
@@ -437,9 +438,7 @@ async def exec(
     if tty is None:
         tty = root.tty
     _check_tty(root, tty)
-    # 0 is zero timeout (expired), need convertion to None
-    async with async_timeout.timeout(timeout or None):
-        await process_exec(root, job, real_cmd, tty)
+    await process_exec(root, job, real_cmd, tty)
 
 
 @command()
