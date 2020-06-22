9501f55932e47fa8b873d830e7303f5b1664184a
Serhiy Storchaka <storchaka@gmail.com>
2020-06-22 15:17:30 +0300

    Do not disable default patterns by options --exclude and --include

--- tests/cli/test_storage.py
+++ tests/cli/test_storage.py
@@ -38,6 +38,25 @@ async def test_calc_filters_user_spec(
         )
 
 
+async def test_calc_filters_user_spec_and_options(
+    monkeypatch: Any, tmp_path: Path, make_client: _MakeClient
+) -> None:
+
+    async with make_client("https://example.com") as client:
+        monkeypatch.chdir(tmp_path)
+        local_conf = tmp_path / ".neuro.toml"
+        local_conf.write_text(
+            toml.dumps({"storage": {"cp-exclude": ["*.jpg", "!main.jpg"]}})
+        )
+        filters = ((False, "a*.jpg"), (True, "temp/"))
+        assert await calc_filters(client, filters) == (
+            (True, "*.jpg"),
+            (False, "main.jpg"),
+            (False, "a*.jpg"),
+            (True, "temp/"),
+        )
+
+
 async def test_calc_ignore_file_names_default(
     monkeypatch: Any, tmp_path: Path, make_client: _MakeClient
 ) -> None:
