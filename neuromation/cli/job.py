3c5e452f021641d91b011d4a9fb7c7192e04247f
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-07-10 18:20:13 +0300

    Hide submit command

--- neuromation/cli/job.py
+++ neuromation/cli/job.py
@@ -145,7 +145,7 @@ def job() -> None:
     """
 
 
-@command(context_settings=dict(allow_interspersed_args=False))
+@command(context_settings=dict(allow_interspersed_args=False), hidden=True)
 @argument("image", type=ImageType())
 @argument("cmd", nargs=-1, type=click.UNPROCESSED)
 @option(
