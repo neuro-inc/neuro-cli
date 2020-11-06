1af717da651d287be8ee93390605bcf81fa36da7
Roman Skurikhin <romasku135@gmail.com>
2020-11-06 17:24:52 +0200

    Allow to remove multiple disks at once

--- tests/e2e/test_e2e_disks.py
+++ tests/e2e/test_e2e_disks.py
@@ -29,3 +29,35 @@ def test_create_get_list_delete(
     cap = helper.run_cli(["disk", "ls"])
     assert cap.err == ""
     assert disk_id not in cap.out
+
+
+@pytest.mark.e2e
+def test_delete_multiple_disks(
+    helper: Helper, disk_factory: Callable[[str], ContextManager[str]]
+) -> None:
+    disk_id_1, disk_id_2 = None, None
+    try:
+        cap = helper.run_cli(["disk", "create", "1G"])
+        assert cap.err == ""
+        disk_id_1, *_ = cap.out.splitlines()[1].split()
+
+        cap = helper.run_cli(["disk", "create", "1G"])
+        assert cap.err == ""
+        disk_id_2, *_ = cap.out.splitlines()[1].split()
+
+        cap = helper.run_cli(["disk", "rm", disk_id_1, disk_id_2])
+        assert cap.err == ""
+
+        cap = helper.run_cli(["disk", "ls"])
+        assert cap.err == ""
+        assert disk_id_1 not in cap.out
+        assert disk_id_2 not in cap.out
+
+    except Exception as e:
+        try:
+            if disk_id_2 is not None:
+                helper.run_cli(["disk", "rm", disk_id_1])
+        finally:
+            if disk_id_1 is not None:
+                helper.run_cli(["disk", "rm", disk_id_1])
+        raise e
