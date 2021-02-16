76bc71ce7dc2f97f880649245ae4c9b074de618e
romasku <roman.skurikhin@neu.ro>
2021-02-16 16:15:50 +0200

    Add life span ends to job status (#1994)
    * Add life span ends to job status
    
    * Added changelog

--- neuro-cli/src/neuro_cli/formatters/jobs.py
+++ neuro-cli/src/neuro_cli/formatters/jobs.py
@@ -54,6 +54,19 @@ def fmt_status(status: JobStatus) -> Text:
     return Text(status.value, style=color)
 
 
+def _get_run_time(job: JobDescription) -> datetime.timedelta:
+    run_time = datetime.timedelta()
+    prev_time: Optional[datetime.datetime] = None
+    for item in job.history.transitions:
+        if prev_time:
+            run_time += item.transition_time - prev_time
+        prev_time = item.transition_time if item.status.is_running else None
+    if prev_time:
+        # job still running
+        run_time += datetime.datetime.now(datetime.timezone.utc) - prev_time
+    return run_time
+
+
 class JobStatusFormatter:
     def __init__(
         self, uri_formatter: URIFormatter, datetime_formatter: DatetimeFormatter
@@ -211,6 +224,21 @@ class JobStatusFormatter:
             assert job_status.history.started_at is not None
             table.add_row(
                 "Started", self._datetime_formatter(job_status.history.started_at)
+            )
+        if (
+            job_status.status
+            in [
+                JobStatus.PENDING,
+                JobStatus.RUNNING,
+                JobStatus.SUSPENDED,
+            ]
+            and job_status.life_span
+        ):
+            life_span = datetime.timedelta(seconds=job_status.life_span)
+            runtime_left = life_span - _get_run_time(job_status)
+            runtime_ends = datetime.datetime.now(datetime.timezone.utc) + runtime_left
+            table.add_row(
+                "Life span ends (approx)", self._datetime_formatter(runtime_ends)
             )
         if job_status.status in [
             JobStatus.CANCELLED,
