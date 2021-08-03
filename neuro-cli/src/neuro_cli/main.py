d97c689e6eea796abad7ac3f2722c4e759d80222
Yevhenii Semendiak <semendyak@gmail.com>
2021-08-03 18:13:01 +0300

    disable version check via env var

--- neuro-cli/src/neuro_cli/main.py
+++ neuro-cli/src/neuro_cli/main.py
@@ -364,6 +364,7 @@ def print_options(
 @option(
     "--disable-pypi-version-check",
     is_flag=True,
+    default=click.BOOL(os.environ.get("NEURO_CLI_DISABLE_PYPI_VERSION_CHECK", False)),
     help="Don't periodically check PyPI to determine whether a new version of "
     "Neuro Platform CLI is available for download.",
 )
