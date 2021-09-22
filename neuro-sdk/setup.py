bddc2386ec2b84f660a98c5d33fc9b41ca6b4124
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-09-22 22:40:05 +0300

    Don't install buggy azure client

--- neuro-sdk/setup.py
+++ neuro-sdk/setup.py
@@ -25,7 +25,7 @@ setup(
         "certifi",
         "toml>=0.10.0",
         "aiobotocore>=1.3.3",
-        "azure-storage-blob>=12.8.1",
+        "azure-storage-blob>=12.8.1,!=12.9.0",
         "google-auth>=2.0.2",
     ],
     include_package_data=True,
