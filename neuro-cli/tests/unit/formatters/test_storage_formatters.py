daec8c74e471ac44deb79d2c8ec8fbc6dc7a13ee
Ivan Zubenko <vanya.zubenko@gmail.com>
2022-02-07 18:36:20 +0200

    add test

--- neuro-cli/tests/unit/formatters/test_storage_formatters.py
+++ neuro-cli/tests/unit/formatters/test_storage_formatters.py
@@ -421,3 +421,10 @@ class TestUsageFormatter:
         )
         formatter = DiskUsageFormatter()
         rich_cmp(formatter(usage))
+
+    def test_org_formatter(self, rich_cmp: Any) -> None:
+        usage = DiskUsageInfo(
+            total=100000, used=80000, free=20000, cluster_name="default", org_name="org"
+        )
+        formatter = DiskUsageFormatter()
+        rich_cmp(formatter(usage))
