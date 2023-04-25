692d3184b4be6c3f78bf485a02764f824259ace7
Ivan Zubenko <ivan.zubenko@angloamerican.com>
2023-04-25 11:00:35 +0300

    refactor

--- neuro-sdk/src/neuro_sdk/_jobs.py
+++ neuro-sdk/src/neuro_sdk/_jobs.py
@@ -1131,10 +1131,8 @@ def _job_telemetry_from_api(value: Dict[str, Any]) -> JobTelemetry:
 
 
 def _volume_to_api(volume: Volume, config: Config) -> Dict[str, Any]:
-    if not config.project_name:
-        raise RuntimeError("Project is not configured")
     uri = normalize_storage_path_uri(
-        volume.storage_uri, config.project_name, config.cluster_name, config.org_name
+        volume.storage_uri, config.project_name_or_raise, config.cluster_name, config.org_name
     )
     resp: Dict[str, Any] = {
         "src_storage_uri": str(uri),
