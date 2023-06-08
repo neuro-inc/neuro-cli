951cda3078618ca535d15946460c746431e7a077
Yevhenii Semendiak <semendyak@gmail.com>
2023-06-09 01:14:33 +0300

    fix typo in mkcredentials (#2997)

--- neuro-cli/src/neuro_cli/blob_storage.py
+++ neuro-cli/src/neuro_cli/blob_storage.py
@@ -1141,7 +1141,7 @@ async def mkcredentials(
     name: Optional[str] = None,
     cluster: Optional[str] = None,
     org: Optional[str] = None,
-    project_name: Optional[str] = None,
+    project: Optional[str] = None,
     read_only: bool = False,
 ) -> None:
     """
@@ -1154,7 +1154,7 @@ async def mkcredentials(
             client=root.client,
             cluster_name=cluster,
             org_name=org_name,
-            project_name=project_name,
+            project_name=project,
         )
         for bucket in buckets
     ]
