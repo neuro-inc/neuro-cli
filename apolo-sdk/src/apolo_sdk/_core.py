f3f638683ccc29822437974e8368b9d293d2ef3b
costefan <konstantine.ovchynnikov@apolo.us>
2024-11-21 17:47:29 +0000

    removed unused ignore type

--- apolo-sdk/src/apolo_sdk/_core.py
+++ apolo-sdk/src/apolo_sdk/_core.py
@@ -200,7 +200,7 @@ class _Core:
                 abs_url,
                 headers=real_headers,
                 heartbeat=heartbeat,
-                timeout=aiohttp.ClientWSTimeout(timeout),  # type: ignore
+                timeout=aiohttp.ClientWSTimeout(timeout),
                 receive_timeout=receive_timeout,
             ) as ws:
                 yield ws
